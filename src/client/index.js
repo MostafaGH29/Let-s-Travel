5377c53a5fa193f79a1cbc2165c7b81e7c9fe0bd
Mostafa Al-Ghazal <mustaphaarsenal9@gmail.com>
2020-02-12 13:28:41 +0200

    First commit

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { addTrip } from "./js/app.js"
+import { checkInput } from "./js/inputCheck.js"
+import { addTripEvList } from "./js/app.js"
+import './styles/main.scss'
+import './views/img/net.png'
+import './views/img/travelearth.png'
+
+
+export { addTrip }
+export { checkInput }
+export { addTripEvList }
+
